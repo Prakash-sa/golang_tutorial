23b49d33db84099e1de20872559fac52f7fb0fa5
Prakash-sa <sainiprakash525@gmail.com>
2023-04-03 20:31:25 -0500

    added interface

---
+++ interfaces.go
@@ -0,0 +1,117 @@
+package main
+
+/*
+//console writer
+
+
+func main() {
+	var w Writer = ConsoleWriter{}
+	w.Write([]byte("Hello Go!"))
+}
+
+type Writer interface {
+	Write([]byte) (int, error)
+}
+
+type ConsoleWriter struct{}
+
+func (cw ConsoleWriter) Write(data []byte) (int, error) {
+	n, err := fmt.Println(string(data))
+	return n, err
+}
+*/
+
+/*
+
+Increment counter
+
+func main() {
+	myInt := IntCounter(0)
+
+	var inc Incrementer = &myInt
+	for i := 0; i < 10; i++ {
+		fmt.Println(inc.Increment())
+	}
+}
+
+type Incrementer interface {
+	Increment() int
+}
+
+type IntCounter int
+
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
+}
+
+
+*/
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	var wc WriterCloser = NewBufferWriterCloser()
+	wc.Write([]byte("Hello YouTube listeners, this is a test"))
+	wc.Close()
+
+	bwc := wc.(*BufferedWriterCloser)
+	fmt.Println(bwc)
+}
+
+type Writer interface {
+	Write([]byte) (int, error)
+}
+
+type Closer interface {
+	Close() error
+}
+
+type WriterCloser interface {
+	Writer
+	Closer
+}
+
+type BufferedWriterCloser struct {
+	buffer *bytes.Buffer
+}
+
+func (bwc *BufferedWriterCloser) Write(data []byte) (int, error) {
+	n, err := bwc.buffer.Write(data)
+	if err != nil {
+		return 0, err
+	}
+
+	v := make([]byte, 8)
+	for bwc.buffer.Len() > 8 {
+		_, err := bwc.buffer.Read(v)
+		if err != nil {
+			return 0, err
+		}
+		_, err = fmt.Println(string(v))
+		if err != nil {
+			return 0, err
+		}
+	}
+	return n, nil
+}
+
+func (bwc *BufferedWriterCloser) Close() error {
+	for bwc.buffer.Len() > 0 {
+		data := bwc.buffer.Next(8)
+		_, err := fmt.Println(string(data))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func NewBufferWriterCloser() *BufferedWriterCloser {
+	return &BufferedWriterCloser{
+		buffer: bytes.NewBuffer([]byte{}),
+	}
+}
